c39fd93a38212a641e39f2c50ac7177039e21b15
corbooo <corbinm@pobox.com>
2026-05-13 12:22:13 -0500

    Connect backend to API-Football

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+const API_FOOTBALL_URL = "https://v3.football.api-sports.io";
 
 app.get("/api/health", (req, res) => {
     res.json({
@@ -15,6 +16,26 @@ app.get("/api/health", (req, res) => {
         message: "StatSide backend is running",
     });
 });
+
+app.get("/api/countries", async (req, res) => {
+    try {
+        const response = await fetch(`${API_FOOTBALL_URL}/countries`, {
+            headers: {
+                "x-apisports-key": process.env.API_FOOTBALL_KEY,
+            },
+        });
+
+        const data = await response.json();
+
+        res.json(data);
+    } catch (error) {
+        console.error("API-Football error:", error.message);
+
+        res.status(500).json({
+            error: "Failed to fetch coutnries from API-Football"
+        });
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`StatSide backend running on port ${PORT}`);
